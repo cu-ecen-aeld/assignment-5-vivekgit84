1d16c63378fbf579f26ce8002031f90b3456f5e1
vtewari <vtewari@Ubuntu.myguest.virtualbox.org>
2024-12-29 23:04:43 -0700

    update

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_DL_DIR=${DL_DIR}
+		make -C buildroot O=${OUTPUT_DIR} defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_DL_DIR=${DL_DIR}
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
