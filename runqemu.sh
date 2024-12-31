b1ffbf028c8d20018f5b0ed6bfeb10519886ce07
vtewari <vtewari@Ubuntu.myguest.virtualbox.org>
2024-12-31 16:18:30 -0700

    update build and qemu script

--- runqemu.sh
+++ runqemu.sh
@@ -6,9 +6,9 @@
 qemu-system-aarch64 \
     -M virt  \
     -cpu cortex-a53 -nographic -smp 1 \
-    -kernel ../../output/images/Image \
+    -kernel buildroot/output/images/Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
     -netdev user,id=eth0,hostfwd=tcp::10022-:22 \
     -device virtio-net-device,netdev=eth0 \
-    -drive file=../../output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
+    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
